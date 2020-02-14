ba29b34d7decd5a2c9c136efa00ad375d0dea210
Jason-Paprocki <jasonpaprocki@protonmail.com>
2020-02-14 11:50:13 -0500

    cant concentrate

--- SimpleMovingAverageTrade/dayTrading.py
+++ SimpleMovingAverageTrade/dayTrading.py
@@ -6,17 +6,29 @@
 def dayTrading(json, timeNow):
 
    #opens the json file with the logs
-   with open('dayTradeSellLogs.json') as json_file:
+   with open('.json') as json_file:
       logsDictionary = json.load(json_file)
 
    #YYYY-MM-DD
    dateNow = timeNow[0:10]
 
-   date5DaysAgo = timeNow[8:10]
+   date5DaysAgo = timeNow[8:10] - 4
+   month5DaysAgo = str(timeNow[5:7])
 
-   if (str(timeNow[5:7]) == "01" or str(timeNow[5:7]) == "03" or 
-   str(timeNow[5:7]) ==  "05" or str(timeNow[5:7]) ==  "07" or 
-   str(timeNow[5:7]) ==  "08" or str(timeNow[5:7]) ==  "10" or 
-   str(timeNow[5:7]) == "12"):
-      if(date5DaysAgo < 1):
-         date5DaysAgo += 30
+   if(date5DaysAgo < 1):
+      month5DaysAgo -= 1
+
+      if (month5DaysAgo == "01" or month5DaysAgo == "03" or 
+      month5DaysAgo ==  "05" or month5DaysAgo ==  "07" or 
+      month5DaysAgo ==  "08" or month5DaysAgo ==  "10" or 
+      month5DaysAgo == "12"):
+            date5DaysAgo = dateNow + 31
+      
+      elif (month5DaysAgo == "04" or month5DaysAgo == "06" or month5DaysAgo ==  "09" or 
+      month5DaysAgo ==  "11" or month5DaysAgo ==  "10" or month5DaysAgo == "12"):
+            date5DaysAgo = dateNow + 30
+      
+      else:
+         date5DaysAgo = dateNow + 29
+
+   
